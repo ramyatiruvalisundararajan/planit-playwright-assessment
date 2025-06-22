aa46907f8ea4598217f572cddad995323351540b
ramyasundararajan <159774916+ramyasundararajan@users.noreply.github.com>
2025-06-22 21:48:55 +0100

    Update Playwright config for headless & update Jenkinsfile for reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,9 +6,12 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  
 
-  reporter: [['list'], ['allure-playwright']],
+  reporter: [
+    ['list'],
+    ['allure-playwright'],
+    ['junit', { outputFile: 'test-results/results.xml' }]  
+  ],
 
   use: {
     baseURL: 'http://jupiter.cloud.planittesting.com',
